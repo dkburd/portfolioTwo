8817481d462c7ee94d3528ce0f02486a7e4dcefc
dkburd <33463001+dkburd@users.noreply.github.com>
2020-12-26 23:45:04 -0800

    various changes

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function showLinks() {
     x.style.display = "block";
   }
 }
+
+window.onscroll = function() {stickNav()};
+
+var navbar = document.getElementById("nav");
+var sticky = navbar.offsetTop;
+
+function stickNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    navbar.classList.remove("nav");
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.classList.add("nav");
+  }
+}
+
+
+    window.formbutton = window.formbutton || function () { (formbutton.q = formbutton.q || []).push(arguments) };
+    formbutton("create", { action: "https://formspree.io/f/xbjppkpb" })
